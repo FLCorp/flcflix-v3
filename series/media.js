6c1943a9714775b518208e01c93d8b23a2535f4e
FLCorporation <108215519+FLCorp@users.noreply.github.com>
2024-02-03 01:42:53 +0100

    Add files via upload

--- series/media.js
+++ series/media.js
@@ -1,4 +1,4 @@
 function changeMedia(videoId) {
     const mediaPlayer = document.getElementById('media-player');
-    mediaPlayer.src = `https://uqload.io/embed-${videoId}`;
+    mediaPlayer.src = `https://uqload.to/embed-${videoId}`;
 }
